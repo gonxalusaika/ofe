d5bfe64f3fbad1dc4cd440b37c480a98c554c3c2
Gonzalo Lema <gonzalo.lema1@gmail.com>
2014-10-14 19:19:56 -0200

    imagenes en dropbox

--- Gemfile
+++ Gemfile
@@ -43,3 +43,4 @@ gem 'pg', group: :production
 gem 'cocoon'
 gem 'quiet_assets', group: :development
 gem 'paperclip', "~> 3.5.3"
+gem "paperclip-dropbox", ">= 1.1.7"
--- app/models/dinosaurio.rb
+++ app/models/dinosaurio.rb
@@ -1,7 +1,8 @@
 class Dinosaurio < ActiveRecord::Base
 	belongs_to :periodo
 
-	has_attached_file :icono, default_url: 'assets/dino-icon.jpg'
+	has_attached_file :icono, default_url: 'assets/dino-icon.jpg',
+		storage: :dropbox, dropbox_credentials: Rails.root.join("config/dropbox.yml")
 	validates_attachment :icono,
   		:content_type => { :content_type => ["image/jpeg", "image/gif", "image/png"] }
 
