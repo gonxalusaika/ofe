a44f8d50ef763cf676566f9a4fba972758c940dc
Gonzalo Lema <gonzalo.lema1@gmail.com>
2014-11-23 18:52:40 -0200

    tilde....

--- app/models/validators/validador_preguntas_estacion.rb
+++ app/models/validators/validador_preguntas_estacion.rb
@@ -1,7 +1,7 @@
 class ValidadorPreguntasEstacion < ActiveModel::Validator
 	def validate(record)
 		record.preguntas.each do |pregunta|
-			record.errors[:base] << "La pregunta #{pregunta.contenido} no está asociada al dinosaurio" unless record.dinosaurio.id == pregunta.dinosaurio_id
+			record.errors[:base] << "La pregunta #{pregunta.contenido} no esta asociada al dinosaurio" unless record.dinosaurio.id == pregunta.dinosaurio_id
 		end
 	end
 end
