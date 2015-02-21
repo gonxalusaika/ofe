a50708ae4bce9520692d00e6b2bbf8d4259e18e5
Gonzalo Lema <gonzalo.lema1@gmail.com>
2015-02-21 15:07:57 -0200

    cambio del largo de despuestas a 60

--- app/models/respuesta.rb
+++ app/models/respuesta.rb
@@ -1,7 +1,7 @@
 class Respuesta < ActiveRecord::Base
 	belongs_to :pregunta
 
-	validates :contenido, presence: true, length: {maximum: 49}
+	validates :contenido, presence: true, length: {maximum: 60}
 	validates :feedback, presence: true
 
 	attr_accessor :cantidad_elegida
