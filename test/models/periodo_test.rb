236521f72c7893d3438fd9b353ed55d96289a383
Gonzalo Lema <gonzalo.lema1@gmail.com>
2015-03-05 15:41:11 -0200

    limite en feedback, mensajes de confirmacion en español

--- test/models/periodo_test.rb
+++ test/models/periodo_test.rb
@@ -2,7 +2,87 @@ require 'test_helper'
 
 class PeriodoTest < ActiveSupport::TestCase
   test "normal" do
-  	p = Periodo.new(nombre: "Un período")
+  	p = Periodo.new(nombre: "Un período", descripcion: "aaaaa")
+    assert p.valid?, "Periodo normal"
+  end
+  test "normal20" do
+  	p = Periodo.new(nombre: "Un período", descripcion: "aaaaa")
+    assert p.valid?, "Periodo normal"
+  end
+  test "normal19" do
+  	p = Periodo.new(nombre: "Un período", descripcion: "aaaaa")
+    assert p.valid?, "Periodo normal"
+  end
+  test "normal18" do
+  	p = Periodo.new(nombre: "Un período", descripcion: "aaaaa")
+    assert p.valid?, "Periodo normal"
+  end
+  test "normal17" do
+  	p = Periodo.new(nombre: "Un período", descripcion: "aaaaa")
+    assert p.valid?, "Periodo normal"
+  end
+  test "normal16" do
+  	p = Periodo.new(nombre: "Un período", descripcion: "aaaaa")
+    assert p.valid?, "Periodo normal"
+  end
+  test "normal15" do
+  	p = Periodo.new(nombre: "Un período", descripcion: "aaaaa")
+    assert p.valid?, "Periodo normal"
+  end
+  test "normal14" do
+  	p = Periodo.new(nombre: "Un período", descripcion: "aaaaa")
+    assert p.valid?, "Periodo normal"
+  end
+  test "normal13" do
+  	p = Periodo.new(nombre: "Un período", descripcion: "aaaaa")
+    assert p.valid?, "Periodo normal"
+  end
+  test "normal12" do
+  	p = Periodo.new(nombre: "Un período", descripcion: "aaaaa")
+    assert p.valid?, "Periodo normal"
+  end
+  test "normal11" do
+  	p = Periodo.new(nombre: "Un período", descripcion: "aaaaa")
+    assert p.valid?, "Periodo normal"
+  end
+  test "normal10" do
+  	p = Periodo.new(nombre: "Un período", descripcion: "aaaaa")
+    assert p.valid?, "Periodo normal"
+  end
+  test "normal9" do
+  	p = Periodo.new(nombre: "Un período", descripcion: "aaaaa")
+    assert p.valid?, "Periodo normal"
+  end
+  test "normal8" do
+  	p = Periodo.new(nombre: "Un período", descripcion: "aaaaa")
+    assert p.valid?, "Periodo normal"
+  end
+  test "normal7" do
+  	p = Periodo.new(nombre: "Un período", descripcion: "aaaaa")
+    assert p.valid?, "Periodo normal"
+  end
+  test "normal6" do
+  	p = Periodo.new(nombre: "Un período", descripcion: "aaaaa")
+    assert p.valid?, "Periodo normal"
+  end
+  test "normal5" do
+  	p = Periodo.new(nombre: "Un período", descripcion: "aaaaa")
+    assert p.valid?, "Periodo normal"
+  end
+  test "normal4" do
+  	p = Periodo.new(nombre: "Un período", descripcion: "aaaaa")
+    assert p.valid?, "Periodo normal"
+  end
+  test "normal3" do
+  	p = Periodo.new(nombre: "Un período", descripcion: "aaaaa")
+    assert p.valid?, "Periodo normal"
+  end
+  test "normal2" do
+  	p = Periodo.new(nombre: "Un período", descripcion: "aaaaa")
+    assert p.valid?, "Periodo normal"
+  end
+  test "normal1" do
+  	p = Periodo.new(nombre: "Un período", descripcion: "aaaaa")
     assert p.valid?, "Periodo normal"
   end
 end
